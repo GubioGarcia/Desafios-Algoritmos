c34f6c98bb5244847126bccaa7ddbb1745132bb1
Gubio Garcia <gubiogarcia@gmail.com>
2023-10-31 12:21:08 -0300

    incremento descrição dos desafios

--- SequenciaIJ4.cpp
+++ SequenciaIJ4.cpp
@@ -1,5 +1,21 @@
-#include <iostream>
+/*
+Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+Entrada: Não há nenhuma entrada neste problema.
+Saída: Imprima a sequencia conforme exemplo abaixo.
+Exemplo de Saída
+I=0 J=1
+I=0 J=2
+I=0 J=3
+I=0.2 J=1.2
+I=0.2 J=2.2
+I=0.2 J=3.2
+.....
+I=2 J=?
+I=2 J=?
+I=2 J=?
+*/
 
+#include <iostream>
 int main () {
     for (double i = 0; i <= 2; i += 0.2) {
         for (double j = 1; j <= 3; j++) {
